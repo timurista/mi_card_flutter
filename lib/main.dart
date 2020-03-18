ac56608342cf35997e9e1a85556a2d419ae2c3c9
Tim Urista <timothy.urista@gmail.com>
2020-03-18 07:11:04 -0700

    add styled layout example of cols

--- lib/main.dart
+++ lib/main.dart
@@ -8,30 +8,60 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
         home: Scaffold(
-          appBar: AppBar(
-            backgroundColor: Colors.indigo,
-            title: Text('Demo'),
+      appBar: AppBar(
+        backgroundColor: Colors.indigo,
+        title: Text('Demo'),
+      ),
+      backgroundColor: Colors.teal,
+      body: SafeArea(
+          child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Container(
+            color: Colors.red,
+            child: Text('hello'),
+            padding: EdgeInsets.all(10.0),
+            width: 100,
+            height: double.infinity,
           ),
-          backgroundColor: Colors.teal,
-          body: SafeArea(
-            child: Container(
-              color: Colors.white,
-              child: Text('hello'),
-              margin: EdgeInsets.symmetric(vertical: 50.0, horizontal: 10.0),
-              padding: EdgeInsets.all(20.0),
-              height: 100,
-              width: 100,
-            ),
+          Container(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                      color: Colors.yellow,
+                      padding: EdgeInsets.all(10.0),
+                      height: 100,
+                      child: Text('Container 1')),
+                  Container(
+                    color: Colors.yellow[200],
+                    child: Text('Container 2'),
+                    padding: EdgeInsets.all(10.0),
+                    height: 100,
+                  ),
+                ]),
           ),
-          floatingActionButton: FloatingActionButton(
-            backgroundColor: Colors.indigo,
-            child: Icon(
-              Icons.add,
-            ),
+
+          Container(
+            color: Colors.lightGreenAccent[200],
+            child: Text('Container 3'),
+            padding: EdgeInsets.all(10.0),
+            height: double.infinity,
           ),
-        )
-    );
+//                Container(
+//                  width: double.infinity,
+//                )
+        ],
+      )),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.indigo,
+        child: Icon(
+          Icons.add,
+        ),
+      ),
+    ));
   }
 }
