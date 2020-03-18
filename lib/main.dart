d73fde092ed90e33782134f4d050df7330718af1
Tim Urista <timothy.urista@gmail.com>
2020-03-18 09:01:45 -0700

    add main card layout for personal info

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+//import 'package:url_launcher/url_launcher.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,59 +10,140 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.indigo,
-        title: Text('Demo'),
-      ),
-      backgroundColor: Colors.teal,
+      backgroundColor: Colors.transparent,
       body: SafeArea(
-          child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          Container(
-            color: Colors.red,
-            child: Text('hello'),
-            padding: EdgeInsets.all(10.0),
-            width: 100,
-            height: double.infinity,
-          ),
-          Container(
-            child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  Container(
-                      color: Colors.yellow,
-                      padding: EdgeInsets.all(10.0),
-                      height: 100,
-                      child: Text('Container 1')),
-                  Container(
-                    color: Colors.yellow[200],
-                    child: Text('Container 2'),
-                    padding: EdgeInsets.all(10.0),
-                    height: 100,
-                  ),
-                ]),
-          ),
 
-          Container(
-            color: Colors.lightGreenAccent[200],
-            child: Text('Container 3'),
-            padding: EdgeInsets.all(10.0),
-            height: double.infinity,
+        child: Container(
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomRight,
+                  colors: <Color>[
+                    Colors.deepPurple,
+                    Colors.lightBlue
+                  ])
           ),
-//                Container(
-//                  width: double.infinity,
-//                )
-        ],
-      )),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.indigo,
-        child: Icon(
-          Icons.add,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 55.0,
+                backgroundColor: Colors.red,
+                backgroundImage: AssetImage("images/timurista-profile.jpeg"),
+              ),
+              Text("Tim Urista", style: TextStyle(
+                fontSize: 40,
+                    color: Colors.white,
+                fontFamily: "Pacifico",
+                letterSpacing: 2.1,
+                fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text("FULLSTACK ENGINEER", style: TextStyle(
+                fontSize: 20,
+                letterSpacing: 2.5,
+                color: Colors.indigoAccent[100],
+                fontFamily: "Comfortaa",
+              ),
+              ),
+
+              SizedBox(height: 20, width: 150.0,
+                child: Divider(
+                color: Colors.lightBlueAccent[100],
+              ),),
+
+
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 24 ),
+                  child: ListTile(
+                    leading:  Icon(
+                      Icons.phone,
+                      color: Colors.indigo,
+                    ),
+                    title: Text("+1 520 349 6601",
+                      style: TextStyle(
+                      color: Colors.indigo,
+                    ),
+                  ),
+                  )
+                ),
+
+              Card(
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 24 ),
+                  child: ListTile(
+                    leading:  Icon(
+                      Icons.email,
+                      color: Colors.indigo,
+                    ),
+                    title: Text("timothy.urista@gmail.com",
+                      style: TextStyle(
+                        color: Colors.indigo,
+                      ),
+                    ),
+                  )
+              ),
+
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 8),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: <Widget>[
+                    Icon(
+                      Icons.web,
+                      color: Colors.indigo,
+                    ),
+                    Icon(
+                      Icons.cloud_queue,
+                      color: Colors.indigo,
+                    ),
+                    Icon(
+                      Icons.mobile_screen_share,
+                      color: Colors.indigo,
+                    ),
+                    Icon(
+                      Icons.brush,
+                      color: Colors.indigo,
+                    )
+                  ],
+                ),
+              ),
+
+//              Text("places I have worked:", style: TextStyle(color: Colors.white),),
+//              Container(
+//                width: double.infinity,
+//                margin: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+//                child: Row(
+//                  mainAxisAlignment: MainAxisAlignment.center,
+//                  crossAxisAlignment: CrossAxisAlignment.center,
+//                  children: <Widget>[
+//                    CircleAvatar(
+//                      radius: 16,
+//                      backgroundColor: Colors.deepPurple,
+//                      backgroundImage: NetworkImage("https://lh3.googleusercontent.com/9VvTJ-UIGlggVvaEr-HvOepHLWCU2DtKs08KKJnWwfBkw60W7tOUGCMwp5Sc-VBA4Q=w300"),
+//                    ),
+//                    SizedBox(
+//                      width: 10,
+//                    ),
+//                    CircleAvatar(
+//                      radius: 16,
+//                      backgroundColor: Colors.blueAccent,
+//                      backgroundImage: NetworkImage("https://facebookbrand.com/wp-content/uploads/2019/04/f_logo_RGB-Hex-Blue_512.png?w=512&h=512"),
+//                    ),
+//                  ],
+//                ),
+//              ),
+
+              Container(
+                width: double.infinity,
+                margin: EdgeInsets.all(10.0),
+              )
+            ],
+          ),
         ),
-      ),
-    ));
+
+      )
+      )
+    );
   }
 }
+
